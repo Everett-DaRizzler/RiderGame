642950eff408a07b1ab4028b81243b3cf2610954
Everett-DaRizzler <everettpope77@gmail.com>
2026-06-07 20:39:14 -0700

    Level_5 and some code
    Completed level 5 and generated some code so that the car can actually die.

--- RiderGame/Assets/Scripts/CarController.cs
+++ RiderGame/Assets/Scripts/CarController.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarController : MonoBehaviour
 {
     bool move = false;
     bool isGrounded = false;
+    Vector2 groundNormal = Vector2.up;
 
     public Rigidbody2D rb;
 
     public float speed = 20f;
     public float rotationSpeed = 2f;
+    public float upsideDownThreshold = -0.5f;
 
     private void Update()
     {
@@ -23,6 +26,11 @@ public class CarController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (isGrounded == true && Vector2.Dot(transform.up, groundNormal) < upsideDownThreshold)
+        {
+            RestartCurrentScene();
+        }
+
         if (move == true)
         {
             if (isGrounded == true)
@@ -35,18 +43,39 @@ public class CarController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        UpdateGroundedState(collision);
         isGrounded = true;
-        
     }
 
-    private void OnCollisionExit2D()
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        UpdateGroundedState(collision);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
         
         isGrounded = false;
         
+    }
+
+    private void RestartCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void UpdateGroundedState(Collision2D collision)
+    {
+        Vector2 normalTotal = Vector2.zero;
+
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            normalTotal += collision.GetContact(i).normal;
+        }
+
+        groundNormal = normalTotal.normalized;
     }
 
     
